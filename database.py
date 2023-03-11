4c04cc9f28ab3f91c20af33c3c2067ca0c5c53f5
DucAnh2611 <117668851+DucAnh2611@users.noreply.github.com>
2023-03-11 20:11:08 +0700

    Update database.py

--- database.py
+++ database.py
@@ -6,20 +6,14 @@ import string
 connect = sqlite3.connect('DiemSinhVien_Python_K2N2_test.db')
 c = connect.cursor()
 
-
 c.execute('''DROP TABLE IF EXISTS "subject_student" ''')
 c.execute('''DROP TABLE IF EXISTS "subject" ''')
 c.execute('''DROP TABLE IF EXISTS "student" ''')
 c.execute('''DROP TABLE IF EXISTS "class" ''')
 
-classRoom = 6
 startGrade = 10
 endGrade = 12
-listClass  = []
-for eachGrade in range (startGrade, endGrade+1):
-    for eachClass in range (1, classRoom+1):
-        listClass.append([f'{eachGrade}A{eachClass}', f'{eachGrade}'])
-    
+classRoom = 6
 c.execute('''
     CREATE TABLE IF NOT EXISTS "class" (
         classId INTEGER ,
@@ -30,55 +24,46 @@ c.execute('''
     )
 ''')
 
-c.executemany('''INSERT INTO "class"("className","grade") VALUES (?, ?)''', listClass)
-
-
-student = 20
-listStudent = []
-listStudentLeftClass = []
-studentEachClass = 30
-for classNum in range(0, classRoom):
-    listStudentLeftClass.append(30)
-    
-
-def classIsReadyForStudent(studentLeftArray, classRoom):
-    classGetIn = int(np.floor(classRoom * np.round(np.random.random(), 2)) )
-    if(studentLeftArray[classGetIn] == 0):
-        allFull = True
-        for classNum in studentLeftArray:
-            if(studentLeftArray[classNum] != 0):
-                allFull = False
-        if(allFull == False):
-            classIsReadyForStudent(studentLeftArray, classRoom)
-        else: 
-            return 0
-    else:
-        return classGetIn    
-
-middleNameList = ["Hoàng", "Văn", "Quang", "Quốc", "Thị", "Minh"]
-firstNameList = ["Nguyễn", "Trần", "Lê", "Phạm", "Hoàng", "Huỳnh", "Phan", "Vũ", "Võ", "Đặng", "Bùi", "Đỗ", "Hồ", "Ngô", "Dương" ,"Lý"]
-
-for eachstudent in range (0, student):
-    roomReady = classIsReadyForStudent(listStudentLeftClass, classRoom)
-    if(roomReady !=0):
-        listStudentLeftClass[roomReady] = listStudentLeftClass[roomReady] -1;
-        firstName = firstNameList[int(np.floor(len(firstNameList) * np.random.random()))]
-        middleName =  middleNameList[int(np.floor(len(middleNameList) * np.random.random()))]
-        lastName = random.choices(string.ascii_uppercase, k=1)
-        listStudent.append([f'{firstName} {middleName} {lastName[0]}', roomReady])
-        
+for eachGrade in range (startGrade, endGrade+1):
+    for eachClass in range (1, classRoom+1):
+        c.execute('''INSERT INTO "class"("className","grade") VALUES (?, ?)''', [f'{eachGrade}A{eachClass}', f'{eachGrade}'])
+ 
 c.execute('''
     CREATE TABLE IF NOT EXISTS "student" (
         studentId INTEGER ,
         studentName varchar(50) NOT NULL,
-        classId int not null,
+        classId INTEGER not null,
 
         PRIMARY KEY (studentId AUTOINCREMENT),
         FOREIGN KEY (classId) REFERENCES class(classId)
     )
 ''')
 
-c.executemany('''INSERT INTO "student"("studentName","classId") VALUES (?, ?)''', listStudent)
+studentEachClass = 30
+studentSum = classRoom * studentEachClass
+middleNameList = ["Hoàng", "Văn", "Quang", "Quốc", "Thị", "Minh"]
+firstNameList = ["Nguyễn", "Trần", "Lê", "Phạm", "Hoàng", "Huỳnh", "Phan", "Vũ", "Võ", "Đặng", "Bùi", "Đỗ", "Hồ", "Ngô", "Dương" ,"Lý"]
+
+def classIsReady(classId, studentEachClass, classRoom):
+    studentInClass = c.execute('''SELECT * FROM "student" WHERE classId == ?''', [classId])
+    
+    if(len(studentInClass.fetchall()) < studentEachClass):
+        return classId
+    else:
+        classIdNew = int(np.ceil(classRoom*np.random.random()))
+        studentEachClassNew = studentEachClass
+        classRoomNew = classRoom
+        return classIsReady(classIdNew, studentEachClassNew, classRoomNew)
+def setStudentToClass(classId):
+    firstName = firstNameList[int(np.floor(len(firstNameList) * np.random.random()))]
+    middleName =  middleNameList[int(np.floor(len(middleNameList) * np.random.random()))]
+    lastName = random.choices(string.ascii_uppercase, k=1)
+    c.execute('''INSERT INTO "student"("studentName", "classId") VALUES (? ,?)''', [f'{firstName} {middleName} {lastName[0]}', classId])
+
+for eachstudent in range (0, studentSum):
+    classId = int(np.ceil(classRoom *np.random.random()))
+    setStudentToClass(classIsReady(classId, studentEachClass, classRoom))
+
 c.execute('''
     CREATE TABLE IF NOT EXISTS "subject" (
         subjectId INTEGER , 
@@ -100,17 +85,23 @@ listSubject = [
     ["Thể dục"]
 ]
 c.executemany('''INSERT INTO "subject"("subjectName") VALUES (?)''', listSubject)
+
 c.execute('''
     CREATE TABLE IF NOT EXISTS "subject_student" (
-        "subjectId"	int NOT NULL,
-        "studentid"	int NOT NULL,
-        "subjectPoint"	smallint NOT NULL,
+        "subjectId"	INTEGER NOT NULL,
+        "studentid"	INTEGER NOT NULL,
+        "subjectPoint"	REAL NOT NULL,
         
         FOREIGN KEY("subjectId") REFERENCES "subject"("subjectId"),
         FOREIGN KEY("studentid") REFERENCES "student"("studentid"),
         PRIMARY KEY("subjectId","studentid")
     )
 ''')
-
+for student in range(0, studentSum):
+    subjectLen = c.execute('''SELECT * FROM "subject" ''')
+    for subject in range(0, len(subjectLen.fetchall())):
+        c.execute('''INSERT INTO "subject_student" VALUES (?,?,?) ''', [ subject +1 , student+1 , np.round(np.random.random()*10 , 2)])
+    
+    
 connect.commit()
 connect.close()
